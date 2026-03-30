85faa73b0d83ccac38318ead4c238afeb6d9758a
Khushdeep Kaur <kk1226123@gmail.com>
2026-03-30 09:50:04 +0100

    Day 2: Added Streamlit UI

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-print("🚀 AI Project Started Successfully")
+import streamlit as st
+
+st.title("📄 AI PDF Chatbot")
+st.write("Upload your PDF and ask questions")
+
+# Upload PDF
+uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
+
+# Ask question
+user_question = st.text_input("Ask a question about your PDF")
+
+if uploaded_file is not None:
+    st.success("PDF uploaded successfully!")
+
+if user_question:
+    st.write("🤖 Answer:")
+    st.write("This is where AI response will come")
+
+    
